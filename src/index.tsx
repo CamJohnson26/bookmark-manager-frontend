c3dcd8b20e6e8e0e13ab2cfc946a911ad34ed89f
Cameron Johnson <cameronrossjohnson@gmail.com>
2024-12-23 18:38:07 -0600

    Fix auth0

--- src/index.tsx
+++ src/index.tsx
@@ -26,9 +26,11 @@ root.render(
             domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ''}
             clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? ''}
             authorizationParams={{
-                redirect_uri: window.location.origin
+                redirect_uri: window.location.origin,
+                audience: process.env.REACT_APP_WORKER_API_URL,
             }}
             useRefreshTokens={true}
+            useRefreshTokensFallback={true}
         >
             <RouteBar title={'Cam\'s Books'} routes={[{
                 name: 'Bookmarks',
